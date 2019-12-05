650f1601da33245e22648e880e499258d773192a
Mike Glaz <mikeglaz@yahoo.com>
2019-12-05 12:06:28 -0600

    with Maximilian

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { EventsComponent } from './events/events.component';
+import { EventListComponent } from './events/event-list/event-list.component';
+import { EventItemComponent } from './events/event-list/event-item/event-item.component';
+import { EventDetailComponent } from './events/event-detail/event-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    EventsComponent,
+    EventListComponent,
+    EventItemComponent,
+    EventDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/events/event.model.ts
@@ -0,0 +1,7 @@
+export class Event {
+  constructor(
+    public name: string,
+    public description: string,
+    public date_and_time: Date,
+    public venue: string) {}
+}
